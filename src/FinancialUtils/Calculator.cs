85944710fb2e04830bce73f4ebef930eae341cdd
ALONSO SALGUERO GUZMAN <SALGUEROGA@bccr.fi.cr>
2026-05-11 11:41:44 -0600

    Update Calculator.cs

--- src/FinancialUtils/Calculator.cs
+++ src/FinancialUtils/Calculator.cs
@@ -10,7 +10,7 @@ public static class Calculator
     /// </summary>
     public static decimal Add(decimal a, decimal b) => a + b;
 
-    public static int Add(int a, int b) => a + b;
+  //  public static int Add(int a, int b) => a + b;
 
     /// <summary>
     /// Resta b de a.
